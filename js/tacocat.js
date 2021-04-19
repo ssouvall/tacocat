1fb9b07e847ff3b8a6642b4d482743fb28779b83
Stephen Souvall <stephensouvall@gmail.com>
2021-04-19 14:43:34 -0600

    Add sweetAlert2 for app result

--- js/tacocat.js
+++ js/tacocat.js
@@ -1,18 +1,10 @@
 const tacoBtn = document.getElementById("tacoBtn");
 
-function fadeOut() {
-    gsap.to("#app-form", { display: "none", opacity: 0, duration: 1 })
-};
-
-function fadeIn() {
-    gsap.to(".result", { display: "block", duration: 5, opacity: 1 })
-};
-
 tacoBtn.addEventListener('click', () => {
     const userInput = document.getElementById('tacoCat').value;
     let reverseWord = "";
     let tacoResult = document.getElementById('tacoResult');
-    let comparedInput = userInput.replace(/[^a-z0-9]/gi,'');
+    let comparedInput = userInput.replace(/[^a-z0-9]/gi, '');
     let finalInput = comparedInput.toLowerCase();
     let start = finalInput.length - 1;
 
@@ -21,13 +13,34 @@ tacoBtn.addEventListener('click', () => {
     }
 
     if (finalInput == reverseWord) {
-        tacoResult.innerHTML = `${userInput} is ${reverseWord} backwards. That's a Palindrome!`;
+        // tacoResult.innerHTML = `${userInput} is ${reverseWord} backwards. That's a Palindrome!`;
+        Swal.fire({
+            title: 'That\'s a Palindrome!',
+            text: `${userInput} is ${reverseWord} backwards. That's a Palindrome!`,
+            width: 600,
+            padding: '3em',
+            background: '#fff',
+            backdrop: `
+                rgba(0,0,123,0.4)
+                url("//media.giphy.com/media/sIfLhexLUqwik/giphy.gif")
+                left top
+                no-repeat
+                `
+        })
     } else {
-        tacoResult.innerHTML = `${userInput} is ${reverseWord} backwards. That is not a Palindrome.`
+        // tacoResult.innerHTML = `${userInput} is ${reverseWord} backwards. That is not a Palindrome.`
+        Swal.fire({
+            title: 'That\'s not a Palindrome.',
+            text: `${userInput} is ${reverseWord} backwards. That's not a Palindrome.`,
+            width: 600,
+            padding: '3em',
+            background: '#fff',
+            backdrop: `
+                rgba(0,0,123,0.4)
+                url("//media.giphy.com/media/vPN3zK9dNL236/giphy.gif")
+                left top
+                no-repeat
+                `
+        })
     }
-    fadeOut();
-    fadeIn();
 });
-
-
-
